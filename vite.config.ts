3b9d8e3fd2d2a79f2d665d5e0c6ee75699afbb84
Simon Eriksson <sim.eriksson@outlook.com>
2024-06-01 09:14:01 +0200

    Correct vite base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/chess-repertoire/",
   plugins: [react()],
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
