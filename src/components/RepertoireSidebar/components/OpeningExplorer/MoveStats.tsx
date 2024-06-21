f0eef506bdfd643b24f4cc72c47f6d34044615b6
Simon Eriksson <sim.eriksson@outlook.com>
2024-06-21 17:17:19 +0200

    Prettify move stats

---
+++ src/components/RepertoireSidebar/components/OpeningExplorer/MoveStats.tsx
@@ -0,0 +1,48 @@
+import { RepertoireOpeningExplorerMove } from "@/repertoire/defs.ts";
+import { isOpeningExplorerMove } from "@/components/RepertoireSidebar/components/OpeningExplorer/utils.ts";
+import { PieChart } from "react-minimal-pie-chart";
+
+interface MoveStatsProps {
+  totalGamesForPosition: number;
+  move: RepertoireOpeningExplorerMove;
+}
+
+export const MoveStats = ({ totalGamesForPosition, move }: MoveStatsProps) => {
+  if (!isOpeningExplorerMove(move)) return null;
+
+  const totalGamesForMove = move.white + move.draws + move.black;
+  const gamesPercentage = (totalGamesForMove / totalGamesForPosition) * 100;
+  const whiteWinRate = (move.white / totalGamesForMove) * 100;
+  const drawRate = (move.draws / totalGamesForMove) * 100;
+  const blackWinRate = (move.black / totalGamesForMove) * 100;
+
+  return (
+    <div className="flex gap-2 justify-between items-center">
+      <span className="hover:scale-110" title={`${totalGamesForMove} games`}>
+        {gamesPercentage.toFixed(0)}%
+      </span>
+      <PieChart
+        style={{
+          width: "22px",
+        }}
+        data={[
+          {
+            title: `Draw: ${drawRate.toFixed(2)}%`,
+            value: drawRate,
+            color: "rgb(90, 90, 90)",
+          },
+          {
+            title: `White: ${whiteWinRate.toFixed(2)}%`,
+            value: whiteWinRate,
+            color: "rgb(220, 220, 220)",
+          },
+          {
+            title: `Black: ${blackWinRate.toFixed(2)}%`,
+            value: blackWinRate,
+            color: "rgb(10, 10, 10)",
+          },
+        ]}
+      />
+    </div>
+  );
+};
