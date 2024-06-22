c7b390f6d6fea8d0034522f718a4ca33878caf37
Simon Eriksson <sim.eriksson@outlook.com>
2024-06-22 14:38:07 +0200

    Add shortcuts for setting annotations settings

--- src/hooks/useKeyboardShortcuts.ts
+++ src/hooks/useKeyboardShortcuts.ts
@@ -6,6 +6,8 @@ import {
   selectGoToNextMove,
   selectGoToPreviousMove,
 } from "@/stores/zustand/selectors.ts";
+import { localStorageStore } from "@/stores/localStorageStore.ts";
+import { ANNOTATION_SETTINGS } from "@/repertoire/defs.ts";
 
 export const useKeyboardShortcuts = () => {
   const goToFirstMove = useRepertoireStore(selectGoToFirstMove);
@@ -15,7 +17,7 @@ export const useKeyboardShortcuts = () => {
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if ((event.target as Element).tagName.toLowerCase() === "textarea") {
+      if ((event.target as Element).tagName.toLowerCase() !== "body") {
         return;
       }
 
@@ -27,6 +29,42 @@ export const useKeyboardShortcuts = () => {
         goToLastMove();
       } else if (event.key === "ArrowDown") {
         goToFirstMove();
+      } else if (event.key === "1") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.BRILLIANT,
+        });
+      } else if (event.key === "2") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.GOOD,
+        });
+      } else if (event.key === "3") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.INTERESTING,
+        });
+      } else if (event.key === "4") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.NEUTRAL,
+        });
+      } else if (event.key === "5") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.DUBIOUS,
+        });
+      } else if (event.key === "6") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.BAD,
+        });
+      } else if (event.key === "7") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.BLUNDER,
+        });
+      } else if (event.key === "8") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.NONE,
+        });
+      } else if (event.key === "9") {
+        localStorageStore.upsertSettings({
+          annotationSetting: ANNOTATION_SETTINGS.DONT_SAVE,
+        });
       }
     };
 
