93630982a86eeabe26f63417cf380bd41cf93974
Simon Eriksson <sim.eriksson@outlook.com>
2024-06-27 17:05:50 +0200

    Better tooltip to show move stats

--- src/components/RepertoireSidebar/OpeningExplorer/MoveStats.tsx
+++ src/components/RepertoireSidebar/OpeningExplorer/MoveStats.tsx
@@ -1,6 +1,8 @@
 import { RepertoireOpeningExplorerMove } from "@/repertoire/defs.ts";
 import { isOpeningExplorerMove } from "@/components/RepertoireSidebar/OpeningExplorer/utils.ts";
 import { PieChart } from "react-minimal-pie-chart";
+import { Tooltip } from "@/components/reused/Tooltip/Tooltip.tsx";
+import { WinPercentageBar } from "@/components/RepertoireSidebar/OpeningExplorer/WinPercentageBar.tsx";
 
 interface MoveStatsProps {
   totalGamesForPosition: number;
@@ -17,32 +19,47 @@ export const MoveStats = ({ totalGamesForPosition, move }: MoveStatsProps) => {
   const blackWinRate = (move.black / totalGamesForMove) * 100;
 
   return (
-    <div className="flex gap-2 justify-between items-center">
-      <span className="hover:scale-110" title={`${totalGamesForMove} games`}>
-        {gamesPercentage.toFixed(0)}%
-      </span>
-      <PieChart
-        style={{
-          width: "22px",
-        }}
-        data={[
-          {
-            title: `Draw: ${drawRate.toFixed(2)}%`,
-            value: drawRate,
-            color: "rgb(150, 150, 150)",
-          },
-          {
-            title: `White: ${whiteWinRate.toFixed(2)}%`,
-            value: whiteWinRate,
-            color: "rgb(255, 255, 255)",
-          },
-          {
-            title: `Black: ${blackWinRate.toFixed(2)}%`,
-            value: blackWinRate,
-            color: "rgb(35, 35, 35)",
-          },
-        ]}
-      />
-    </div>
+    <Tooltip
+      tooltip={
+        <>
+          <div className="text-xs font-light mb-2">
+            Total games: {totalGamesForMove}
+          </div>
+          <WinPercentageBar
+            whiteWinRate={whiteWinRate}
+            drawRate={drawRate}
+            blackWinRate={blackWinRate}
+          />
+        </>
+      }
+    >
+      <div className="flex gap-2 justify-between items-center">
+        <span className="hover:scale-110" title={`${totalGamesForMove} games`}>
+          {gamesPercentage.toFixed(0)}%
+        </span>
+        <PieChart
+          style={{
+            width: "22px",
+          }}
+          data={[
+            {
+              title: `Draw: ${drawRate.toFixed(2)}%`,
+              value: drawRate,
+              color: "rgb(150, 150, 150)",
+            },
+            {
+              title: `White: ${whiteWinRate.toFixed(2)}%`,
+              value: whiteWinRate,
+              color: "rgb(255, 255, 255)",
+            },
+            {
+              title: `Black: ${blackWinRate.toFixed(2)}%`,
+              value: blackWinRate,
+              color: "rgb(35, 35, 35)",
+            },
+          ]}
+        />
+      </div>
+    </Tooltip>
   );
 };
