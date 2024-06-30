9bebde5a8c769be40f91079c1f14d2e882a7ac7a
Simon Eriksson <sim.eriksson@outlook.com>
2024-06-30 16:01:01 +0200

    Correct tooltip rendering after table has been scrolled

--- src/components/reused/Tooltip/Tooltip.tsx
+++ src/components/reused/Tooltip/Tooltip.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
 import "./Tooltip.scss";
 
 interface TooltipProps {
@@ -7,12 +8,49 @@ interface TooltipProps {
 }
 
 export const Tooltip = ({ tooltip, children }: TooltipProps) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [coords, setCoords] = useState({ top: 0, left: 0 });
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const openTooltip = () => {
+    if (!containerRef.current) return;
+    const rect = containerRef.current.getBoundingClientRect();
+    setCoords({ top: rect.top, left: rect.left });
+    setShowTooltip(true);
+  };
+
+  const closeTooltip = () => setShowTooltip(false);
+
+  useEffect(() => {
+    if (showTooltip) {
+      const handleScroll = () => {
+        if (!containerRef.current) return;
+        const rect = containerRef.current.getBoundingClientRect();
+        setCoords({ top: rect.top, left: rect.left });
+      };
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
+  }, [showTooltip]);
+
   return (
-    <div className="repertoire-tooltip">
-      <div className="repertoire-tooltip__content border border-primary">
-        {tooltip}
-      </div>
-      <div className="repertoire-tooltip__body">{children}</div>
+    <div
+      ref={containerRef}
+      onMouseEnter={openTooltip}
+      onMouseLeave={closeTooltip}
+    >
+      {children}
+      {showTooltip &&
+        ReactDOM.createPortal(
+          <div
+            className="repertoire-tooltip border border-primary"
+            style={{ position: "fixed", top: coords.top, left: coords.left }}
+          >
+            {tooltip}
+          </div>,
+          document.body,
+        )}
     </div>
   );
 };
