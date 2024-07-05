4412d964d0d9a21dbc2f01bba2012ed7b86d279d
Simon Eriksson <sim.eriksson@outlook.com>
2024-07-05 19:29:14 +0200

    More lenient base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chess-repertoire/",
+  base: "/chess-repertoire",
   plugins: [react()],
   server: {
     host: true,
