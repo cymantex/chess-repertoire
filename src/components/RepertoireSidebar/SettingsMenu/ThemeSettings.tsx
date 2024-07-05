1008391665dfba4cf0c41376cf687b94946abce7
Simon Eriksson <sim.eriksson@outlook.com>
2024-07-05 18:14:03 +0200

    Correct name of disabled site theme option

--- src/components/RepertoireSidebar/SettingsMenu/ThemeSettings.tsx
+++ src/components/RepertoireSidebar/SettingsMenu/ThemeSettings.tsx
@@ -24,7 +24,7 @@ export const ThemeSettings = () => {
         onChange={changeTheme}
         value={theme}
       >
-        <option disabled>Dark site themes</option>
+        <option disabled>Site themes</option>
         {DAISY_UI_THEMES.map((daisyUiTheme) => (
           <option key={daisyUiTheme} value={daisyUiTheme}>
             {daisyUiTheme}
