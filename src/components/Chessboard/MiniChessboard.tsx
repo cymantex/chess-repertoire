5c16a3842a183b24fc80f9cbc0f9526ccbccb56e
Simon Eriksson <sim.eriksson@outlook.com>
2024-07-06 21:53:14 +0200

    Mini board should use same orientation as main board

--- src/components/Chessboard/MiniChessboard.tsx
+++ src/components/Chessboard/MiniChessboard.tsx
@@ -1,12 +1,13 @@
 import { Chessground } from "@/external/chessground/Chessground.tsx";
 import { useRepertoireStore } from "@/stores/zustand/useRepertoireStore.ts";
-import { selectPgn } from "@/stores/zustand/selectors.ts";
+import { selectOrientation, selectPgn } from "@/stores/zustand/selectors.ts";
 import { Chess } from "chess.js";
 import { PGN_HEADERS } from "@/defs.ts";
 import { useId } from "react";
 
 export const MiniChessboard = ({ moves = [] }: { moves?: string[] }) => {
   const pgn = useRepertoireStore(selectPgn);
+  const orientation = useRepertoireStore(selectOrientation);
 
   const fen = pgn.headers.get(PGN_HEADERS.FEN);
   const chess = new Chess(fen);
@@ -16,6 +17,7 @@ export const MiniChessboard = ({ moves = [] }: { moves?: string[] }) => {
     <Chessground
       id={useId()}
       fen={chess.fen()}
+      orientation={orientation}
       coordinates={false}
       chessgroundDivProps={{
         style: {
