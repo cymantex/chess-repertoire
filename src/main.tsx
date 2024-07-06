4c46e7fb3d9c9429f75be47b2005c7abe75024d6
Simon Eriksson <sim.eriksson@outlook.com>
2024-07-06 19:32:23 +0200

    Move fonts to public folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RepertoireApp } from "./RepertoireApp.tsx";
-import "./assets/fonts/fonts.scss";
 import "./index.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
