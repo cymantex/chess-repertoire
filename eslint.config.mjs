26370a459453d56ff3af87998d043046d75c0b53
Simon Eriksson <sim.eriksson@outlook.com>
2024-12-30 18:18:42 +0100

    Add react eslint rules according to new flat config rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,26 @@
 // @ts-check
 
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import reactRefresh from "eslint-plugin-react-refresh";
+import typescriptEslint from "typescript-eslint";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 
+const compat = new FlatCompat();
 const files = ["**/*.ts", "**/*.tsx"];
 
 export default [
-  ...[
-    reactRefresh.configs.recommended,
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-  ].map((conf) => ({
-    ...conf,
-    files,
-  })),
+  eslint.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  pluginReactRefresh.configs.recommended,
+  ...compat.extends("plugin:react-hooks/recommended"),
   {
-    files,
     rules: {
+      "react/display-name": "off",
+      "react/react-in-jsx-scope": "off",
       "@typescript-eslint/array-type": "error",
       "@typescript-eslint/consistent-type-imports": "error",
       "react-refresh/only-export-components": [
@@ -26,5 +29,19 @@ export default [
       ],
       "@typescript-eslint/ban-ts-comment": "off",
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
   },
-];
+].map((conf) => ({
+  ...conf,
+  files,
+}));
